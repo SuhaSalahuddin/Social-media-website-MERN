247e52c947a9fae41770de1ebb6846a17c718a00
SuhaSalahuddin <suha_salahuddin@yahoo.com>
2019-12-14 19:46:41 +0500

    Creating-JWT

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://suha:suha@cluster0-nqeet.mongodb.net/test"
+  mongoURI: "mongodb+srv://suha:suha@cluster0-nqeet.mongodb.net/test",
+  secretOrKey: "secret"
 };
