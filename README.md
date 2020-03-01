323792478ef2427e8417ee8d161ef23172ba2060
SuhaSalahuddin <suha_salahuddin@yahoo.com>
2020-03-01 08:50:49 +0500

    started frontend

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Following a tutorial of traversy media
 
 Download React-DevTools & Redux-DevTools on Goolge Chrome
+
+in client folder - all things related to only React
+npm i react-router-dom
+npm i concurrently
+npm i axios - video 36
+npm i classnames
--- server.js
+++ server.js
@@ -3,36 +3,36 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 
-const posts = require("./routes/api/posts");
-const profile = require("./routes/api/profile");
 const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 
 const app = express();
 
-//BodyParser middle-wear
+// Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// DB config
-var db = require("./config/keys.js").mongoURI;
+// DB Config
+const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB
 mongoose
   .connect(db)
-  .then(() => console.log("Connected to database"))
+  .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
-//Passport middleware
+// Passport middleware
 app.use(passport.initialize());
 
-//Passport Config
-require("./config/passport.js")(passport);
+// Passport Config
+require("./config/passport")(passport);
 
 // Use Routes
-app.use("/api/posts", posts);
-app.use("/api/profile", profile);
 app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
